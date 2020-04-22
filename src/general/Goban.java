c9ed0d75af09ae2f9e2dd0701716343cf44a452c
Amandine <morel.amandine025@gmail.com>
2020-04-22 10:12:42 +0200

    init class goban

--- src/general/Goban.java
+++ src/general/Goban.java
@@ -1,4 +1,13 @@
 package general;
 
+import game.GameRecord;
+
 public class Goban {
+    private final int width;
+
+    private final int height;
+
+    private final Intersection[][] intersections;
+
+    private final GameRecord gameRecord;
 }
