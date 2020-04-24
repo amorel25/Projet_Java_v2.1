679a672f3deeec982cb5782cf55b421cdf2a4609
Amandine <morel.amandine025@gmail.com>
2020-04-24 10:19:34 +0200

    modif goban prévision state

--- src/general/Main.java
+++ src/general/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("hello");
 
-	    Player test = new Player(1);
-	    System.out.println(test);
+	    GameManager jeu = new GameManager();
     }
 }
