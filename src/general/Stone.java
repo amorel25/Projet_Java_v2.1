a7c624ad5b6019886cc6a40a20c3b1cf912aaf5a
Admin <Admin@ADMIN-PC>
2020-05-06 09:02:59 +0200

    modification de la class stone

--- src/general/Stone.java
+++ src/general/Stone.java
@@ -1,5 +1,6 @@
 package general;
 
+import State.GameState;
 import game.Intersection;
 
 import java.util.HashSet;
@@ -7,65 +8,118 @@ import java.util.Set;
 
 public class Stone {
 
-    private final Set<Intersection> stones;
+  private static int idx;
+  private final int id;
+  private final Player colorPlayer;
+  private final int x;
+  private final int y;
+  private final int placeTime;
+  private int prisonTime;
 
-    private final Set<Intersection> liberties;
-
-    private final Player owner;
-
-    public Stone(Set<Intersection> stones, Set<Intersection> liberties, Player owner) {
-        this.stones = stones;
-        this.liberties = liberties;
-        this.owner = owner;
+    public Stone(Player colorPlayer, int x, int y) {
+        this.colorPlayer = colorPlayer;
+        this.x = x;
+        this.y = y;
+        prisonTime = -1;
+        this.id = idx++;
     }
 
-    public Stone(Intersection intersection, Player owner) {
-        stones = new HashSet<Intersection>();
-        stones.add(intersection);
-        this.owner = owner;
-        liberties = new HashSet<Intersection>(intersection.getEmptyNeighbors());
-    }
+    public boolean isPrisoner(){
 
-    public Stone(Stone stoneChain){
-        this.stones = new HashSet<Intersection>(stoneChain.stones);
-        this.liberties = new HashSet<>(stoneChain.liberties);
-        this.owner = stoneChain.owner;
-    }
-
-    public Player getOwner(){
-        return owner;
-    }
-
-    public Set<Intersection> getLiberties(){
-        return liberties;
-    }
-
-    public Set<Intersection> getStones() {
-        return stones;
-    }
-
-    public void addStone(Stone stoneChain, Intersection playedStone){
-        this.stones.addAll(stoneChain.stones);
-        this.liberties.addAll(stoneChain.liberties);
-        this.liberties.remove(playedStone);
+        return prisonTime != -1;
 
     }
 
-    public Stone removeLiberty(Intersection playedStone){
-        Stone newStoneChain = new Stone(stones, liberties, owner);
-        newStoneChain.liberties.remove(playedStone);
-        return newStoneChain;
+    public static int getIdx() {
+        return idx;
     }
 
-    public void die() {
-        for (Intersection rollingStone : this.stones) {
-            rollingStone.setStoneChain(null);
-            Set<Stone> adjacentStoneChains = rollingStone.getAjacentStoneChains();
-            for (Stone stone : adjacentStoneChains) {
-                stone.liberties.add(rollingStone);
-            }
-        }
+    public int getId() {
+        return id;
+    }
 
-        this.owner.addCapturedStones(this.stones.size());
+    public Player getColorPlayer() {
+        return colorPlayer;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getPlaceTime() {
+        return placeTime;
+    }
+
+    public int getPrisonTime() {
+        return prisonTime;
+    }
+
+    public static void setIdxCounter(int idxSet){
+        idx = idxSet;
+    }
+
+    public static void resetIdxCounter(){
+        idx = 0;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((colorPlayer == null) ? 0 : colorPlayer.hashCode());
+        result = prime * result + id;
+        result = prime * result + placeTime;
+        result = prime * result + prisonTime;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Stone other = (Stone) obj;
+        if (colorPlayer != other.colorPlayer)
+            return false;
+        if (id != other.id)
+            return false;
+        if (placeTime != other.placeTime)
+            return false;
+        if (prisonTime != other.prisonTime)
+            return false;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
+    }
+
+    public boolean similar(Stone s) {
+        if (this == s)
+            return true;
+        if (s == null)
+            return false;
+        if (colorPlayer != s.colorPlayer)
+            return false;
+        if (x != s.x)
+            return false;
+        if (y != s.y)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Stone [id=" + id + ", color=" + colorPlayer + ", x=" + x + ", y=" + y + ", placeTime=" + placeTime
+                + ", prisonTime=" + prisonTime + "]";
     }
 }
